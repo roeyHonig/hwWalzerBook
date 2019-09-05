379d8610e6cb5f64aab76aa290c8e690bcdf607a
Roey Honig <roeyhonig94@gmail.com>
2019-09-06 01:27:03 +0300

    class=dropdown-menu is 75% max-height of the window height

--- hwScript.js
+++ hwScript.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     // jQuery methods go here...
     console.log("hello world")
-    
+    var window_height = $(window).height()
+    var dropdown_link_offset = $("#navbarDropdownMenuLink").offset();
+    var y_position = dropdown_link_offset.top + $("#navbarDropdownMenuLink").height();
+    $("#hw-dropdown-menu").css("max-height", 0.75*(window_height-y_position)+"px")
   });

1e6cc2909020c0a8db6f924e26c1a8e6a8f23059
Roey Honig <roeyhonig94@gmail.com>
2019-09-07 19:29:22 +0300

    removed the href attr from the dark \ light toggle link, because it kept jumping to the top of the page

--- hwScript.js
+++ hwScript.js
@@ -6,25 +6,24 @@ $(document).ready(function(){
     var dropdown_link_offset = $("#navbarDropdownMenuLink").offset();
     var y_position = dropdown_link_offset.top + $("#navbarDropdownMenuLink").height();
     $("#hw-dropdown-menu").css("max-height", 0.75*(window_height-y_position)+"px")
+
   });
 
-  function sayHello() {
-    // TODO: toggle dark / light mode
+  function toggler_dark_light_mode() {
     if ($("#hwBookContent").css("color") == "rgb(0, 0, 0)") {
-        // dark mode
-        $("#hwBookContent").css("background-image", "url('Resources/black_paper.png')")
-        $("#hwBookContent").css("color", "white")
-        $(".navbar").removeClass("navbar-dark")
-        $(".navbar").addClass("navbar-light")
-        $(".navbar").css("background-color", "rgba(255, 255, 255, 0.9)")
-        
-    } else {
-        // light mode
-        $("#hwBookContent").css("background-image", "url('Resources/paper_fibers.png')")
-        $("#hwBookContent").css("color", "black")
-        $(".navbar").removeClass("navbar-light")
-        $(".navbar").addClass("navbar-dark")
-        $(".navbar").css("background-color", "rgba(0, 0, 0, 0.9)")
-    }
-    
+      // dark mode
+      $("#hwBookContent").css("background-image", "url('Resources/black_paper.png')")
+      $("#hwBookContent").css("color", "white")
+      $(".navbar").removeClass("navbar-dark")
+      $(".navbar").addClass("navbar-light")
+      $(".navbar").css("background-color", "rgba(255, 255, 255, 0.9)")
+      
+     } else {
+      // light mode
+      $("#hwBookContent").css("background-image", "url('Resources/paper_fibers.png')")
+      $("#hwBookContent").css("color", "black")
+      $(".navbar").removeClass("navbar-light")
+      $(".navbar").addClass("navbar-dark")
+      $(".navbar").css("background-color", "rgba(0, 0, 0, 0.9)")
+     }
   }

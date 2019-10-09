c463b31327c61805f372b836bd8f981515b300c4
Roey Honig <roeyhonig94@gmail.com>
2019-10-09 12:07:51 +0300

    canvas is correctly positioned and with the desired dimensions

--- hwScript.js
+++ hwScript.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
     $("#hw-dropdown-menu").css("max-height", 0.75*(window_height-y_position)+"px")
     var window_width = $(window).width()
     $("#hw-dropdown-menu").css("max-width", 0.75*(window_width)+"px")
+    setOpeningImgDimensionsAccordingToWidtHeightRatioOf(0.5);
   });
 
   function toggler_dark_light_mode() {
@@ -35,9 +36,28 @@ $(document).ready(function(){
     $('.navbar-toggler').each(function(i, obj) {
       // for each element 
       $(this).addClass("collapsed")
-  });
-  $('.navbar-collapse').each(function(i, obj) {
-    // for each element 
-    $(this).removeClass("show")
-});
+    });
+    $('.navbar-collapse').each(function(i, obj) {
+      // for each element 
+      $(this).removeClass("show")
+    });
+  }
+
+  function setOpeningImgDimensionsAccordingToWidtHeightRatioOf(ratio) {
+    var canvasWidthAsString = $("#reverseScaleClockCanvasOpeningImg").css("width")
+    var canvasWidth = parseInt(canvasWidthAsString, 10); // 10 means parse this string in the decimal number system
+    var canvasHeight = (1/ratio)*canvasWidth
+    // set max-height to 1000px
+    if (canvasHeight > 1000) {
+      canvasHeight = 1000; 
+      canvasWidth = ratio*canvasHeight;
+    }
+    $("#reverseScaleClockCanvasOpeningImg").css("height", canvasHeight+"px")
+    $("#reverseScaleClockCanvasOpeningImg").css("width", canvasWidth+"px")
+    $("#reverseScaleClockCanvasOpeningImg").css("visibility", "visible")
+    // center the element
+    var parentDivWidth = $('#reverseScaleClockCanvasOpeningImg').parent().width()
+    var sideMargin = (parentDivWidth-1-1-canvasWidth)/2
+    $("#reverseScaleClockCanvasOpeningImg").css("margin-left", sideMargin+"px")
+    $("#reverseScaleClockCanvasOpeningImg").css("margin-right", sideMargin+"px")
   }

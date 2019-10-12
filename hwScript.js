2c5ea626086ed221fc08643f5e3b900cc7d0d943
Roey Honig <roeyhonig94@gmail.com>
2019-10-13 01:38:26 +0300

    we have a border being drawn in canvas and completely scalable

--- hwScript.js
+++ hwScript.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
     $("#hw-dropdown-menu").css("max-height", 0.75*(window_height-y_position)+"px")
     var window_width = $(window).width()
     $("#hw-dropdown-menu").css("max-width", 0.75*(window_width)+"px")
-    setOpeningImgDimensionsAccordingToWidtHeightRatioOf(0.5);
+    drawTheReverseScaleClock()
+    setOpeningImgDimensionsAccordingToWidtHeightRatioOf(0.4);
   });
 
   function toggler_dark_light_mode() {
@@ -44,6 +45,7 @@ $(document).ready(function(){
   }
 
   function setOpeningImgDimensionsAccordingToWidtHeightRatioOf(ratio) {
+    $("#reverseScaleClockCanvasOpeningImg").css("width", 80+"%") // set the width to be 80% if it results in too big height, we will fix it
     var canvasWidthAsString = $("#reverseScaleClockCanvasOpeningImg").css("width")
     var canvasWidth = parseInt(canvasWidthAsString, 10); // 10 means parse this string in the decimal number system
     var canvasHeight = (1/ratio)*canvasWidth
@@ -57,7 +59,61 @@ $(document).ready(function(){
     $("#reverseScaleClockCanvasOpeningImg").css("visibility", "visible")
     // center the element
     var parentDivWidth = $('#reverseScaleClockCanvasOpeningImg').parent().width()
-    var sideMargin = (parentDivWidth-1-1-canvasWidth)/2
+    var sideMargin = (parentDivWidth-canvasWidth)/2
     $("#reverseScaleClockCanvasOpeningImg").css("margin-left", sideMargin+"px")
     $("#reverseScaleClockCanvasOpeningImg").css("margin-right", sideMargin+"px")
+  }
+  
+  // If we use the canvas element with static dimensions, then we can draw with no problem, but if we need dynamic dimensions that will sclae autometically, according to the parent div element, for example (such as 80% width, in our case), then we have a problem and the canvas element will scale the drawing as well, similarlly to scaling of an image.
+  // In Addition the canvas seems to have a defult dimensions of 300px X 150px with an aspect ratio of 2.
+  // If we just set static dimensions of 400px X 1000px , this will still result in streatching of the drawing in the y direction
+  // this is all because of the need to take into account the pixsel density resulution, each screen has or the Dot Per Inch - dpi
+  // read this artical to explain.
+  // the solution is this method that should be called inside every draw function
+  // https://medium.com/wdstack/fixing-html5-2d-canvas-blur-8ebe27db07da
+  function fix_dpi() {
+    //get DPI
+    let dpi = window.devicePixelRatio;
+    //get canvas
+    let canvas = document.getElementById('reverseScaleClockCanvasOpeningImg');
+    //get context
+    let ctx = canvas.getContext('2d');
+    //get CSS height
+    //the + prefix casts it to an integer
+    //the slice method gets rid of "px"
+    let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+    //get CSS width
+    let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+    //scale the canvas
+    canvas.setAttribute('height', style_height * dpi);
+    canvas.setAttribute('width', style_width * dpi);    
+  }
+
+  function drawTheReverseScaleClock() {
+    // ----------------------------------------------------------------------------------
+    // remember!!!! - you're scaling the canvas, so 400X1000 become 1200X3000, so when you draw multiply each length or coordinate by the dpi
+    // don't worry if these sounds very big, because after we draw we shrink the canvas to an actual fitting size
+    fix_dpi()
+    let dpi = window.devicePixelRatio;
+    // ----------------------------------------------------------------------------------
+    var canvasWidth = $("#reverseScaleClockCanvasOpeningImg").width()
+    var canvasHeight = $("#reverseScaleClockCanvasOpeningImg").height()
+    var canvasElement = document.getElementById('reverseScaleClockCanvasOpeningImg');
+    if (canvasElement.getContext) {
+      // remember!!! what you are drawing here is in a 400 X 1000 canvas but don't forget to multiply by the dpi
+      var ctx = canvasElement.getContext('2d');
+      let lineWidthBeforDpi = 2;
+      let lineWidth = lineWidthBeforDpi*dpi;
+      ctx.lineWidth = lineWidth;
+      ctx.strokeRect(0.5*lineWidth, 0.5*lineWidth, canvasWidth*dpi-lineWidth, canvasHeight*dpi-lineWidth)
+      /*
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(canvasWidth*dpi, 0*dpi);
+    ctx.lineTo(canvasWidth*dpi, canvasHeight*dpi);
+    ctx.lineTo(0*dpi, canvasHeight*dpi);
+    ctx.closePath();
+    ctx.stroke();
+    */
+    }
   }
